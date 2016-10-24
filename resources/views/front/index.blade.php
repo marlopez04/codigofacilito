e37a38ab483a14a929ea4351f9b5bf7c333e4f22
marlopez04 <marlopez04@hotmail.com>
2016-10-24 19:51:00 -0300

    video 33

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -20,12 +20,38 @@
 						</div>
 					</div>
 				</div>
+				<div class="col-md-4 aside">
+					<div class="panel panel-primary">
+					  <div class="panel-heading">
+					    <h3 class="panel-title">Panel primary</h3>
+					  </div>
+					  <div class="panel-body">
+						<ul class="list-group">
+							<li class="list-group-item"><span class="badge">14</span> Cras justo odio</li>
+						  	<li class="list-group-item"><span class="badge">2</span>Dapibus ac facilisis in</li>
+						  	<li class="list-group-item"><span class="badge">1</span>Morbi leo risus</li>
+						</ul>
+
+					  </div>
+					</div>
+
+					<div class="panel panel-primary">
+					  <div class="panel-heading">
+					    <h3 class="panel-title">Panel primary</h3>
+					  </div>
+					  <div class="panel-body">
+						<span class="label label-default">Default</span>
+						<span class="label label-primary">Primary</span>
+						<span class="label label-success">Success</span>
+						<span class="label label-warning">Warning</span>
+						<span class="label label-danger">Danger</span>
+						<span class="label label-info">Info</span>
+					  </div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-
-
-
 
 @endsection
 
